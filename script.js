72576c9f16e20c3efe582df0968b65aa9d636cff
IOrozakunov23 <islamburunduk045>
2022-03-04 14:18:02 +0300

    added 3 files

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('example1 button');
+const span = document.querySelector('example1 span');
+
+span.textContent = localStorage.getItem('example1')
+
+button.addEventListener('click', function() {
+  span.textContent++;
+
+  localStorage.setItem('example1-number', span.textContent);
+});
